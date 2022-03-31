ad17836b2ab03a7a0ff2b574eea95a944a3a0d8f
Domingo Dirutigliano <dirutigliano20@gmail.com>
2022-03-31 19:55:16 +0200

    Lock in Server client table

--- jeeachinto/server.py
+++ jeeachinto/server.py
@@ -7,6 +7,7 @@ from . import utils
 class Server:
     def __init__(self,bind_ip="0.0.0.0",bind_port=4545):
         self.clienttable = {}
+        self.clienttablelock = Lock()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
         self.socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT,1)
@@ -25,17 +26,24 @@ class Server:
         return utils.socket_msg_recv(client)
 
     def send_msg_client(self, client_name, header={}, body=b""):
+        self.clienttablelock.acquire()
         if client_name in self.clienttable:
-            self.clienttable[client_name]["lock"].acquire()
-            self.clienttable[client_name]["conn"].settimeout(utils.TIMEOUT_SOCKS)
+            target = self.clienttable[client_name]
+            self.clienttablelock.release()
+            target["lock"].acquire()
+            target["conn"].settimeout(utils.TIMEOUT_SOCKS)
             try:
-                self.clienttable[client_name]["conn"].sendall(utils.msg_encode(header,body))
+                target["conn"].sendall(utils.msg_encode(header,body))
             finally:
-                self.clienttable[client_name]["conn"].settimeout(None)
-                self.clienttable[client_name]["lock"].release()
+                target["conn"].settimeout(None)
+                target["lock"].release()
+        else:
+            self.clienttablelock.release()
 
     def send_msg_to_client(self, by, to, body):
+        self.clienttablelock.acquire()
         if to in self.clienttable:
+            self.clienttablelock.release()
             self.send_msg_client(to,{
                 "action":"recv",
                 "by": by
@@ -45,13 +53,18 @@ class Server:
                 "status":None
             })
         else:
+            self.clienttablelock.release()
             self.send_msg_client(by,{
                 "action":"send-status",
                 "status":"Target name not subscribed!"
             })
 
     def client_listener(self, client_name):
-        conn = self.clienttable[client_name]["conn"]
+        with self.clienttablelock:
+            try:
+                conn = self.clienttable[client_name]["conn"]
+            except Exception:
+                return
         while True:
             try:
                 header, data = self.recv_msg_client(conn)
@@ -59,11 +72,18 @@ class Server:
                     continue
                 if header["action"] == "send":
                     self.send_msg_to_client(client_name, header["to"], data)
-            except Exception as e:
+            except Exception:
                 utils.pdbg(client_name,"disconnected!")
                 utils.pexc()
-                conn.close()
-                del self.clienttable[client_name]
+                try:
+                    conn.close()
+                except Exception:
+                    pass
+                with self.clienttablelock:
+                    try:
+                        del self.clienttable[client_name]
+                    except Exception:
+                        pass
                 return
                     
     def client_accept_handle(self, client):
@@ -79,10 +99,14 @@ class Server:
             else:
                 name_pc = str(uuid.uuid4())
             
-            if name_pc in self.clienttable:
-                self.clienttable[name_pc]["conn"].close()
-                
-            self.clienttable[name_pc] = {"conn":client, "lock":Lock()}
+            with self.clienttablelock:
+                try:
+                    if name_pc in self.clienttable:
+                        self.clienttable[name_pc]["conn"].close()
+                except Exception:
+                    pass
+                self.clienttable[name_pc] = {"conn":client, "lock":Lock()}
+
             Thread(target=self.client_listener, args=(name_pc,)).start()
             utils.pdbg(name_pc,"connected!")
             client.sendall(utils.msg_encode(
